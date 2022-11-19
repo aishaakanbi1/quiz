e1a9ab02ee0fefc8cc4fec9034bf93596e7303eb
aishaakanbi1 <110837578+aishaakanbi1@users.noreply.github.com>
2022-11-20 00:03:11 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,461 @@
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "What is the highest common factor of the numbers 30 and 132?",
+    "options": [
+    {
+    "a": "6",
+    "b": "25",
+    "c": "8",
+    "d": "16"
+    }
+    ],
+    "answer": "6",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "123+4-5+67-89 = ? ",
+    "options": [
+    {
+    "a": "104",
+    "b": "100",
+    "c": "80"
+    }
+    ],
+    "answer": "100",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "From the number 0 to the number 1000, the letter “A” appears only in?",
+    "options": [
+    {
+    "a": "100",
+    "b": "900",
+    "c": "1000"
+    }
+    ],
+    "answer": "1000",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": " What is next in the following number series: 256, 289, 324, 361 . . . ?",
+    "options": [
+    {
+    "a": "367",
+    "b": "400",
+    "c": "500"
+    }
+    ],
+    "answer": "400",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "At a Christmas party, everyone shook hands with everyone else. There were a total of 66 handshakes that happened during the party. How many people were present? ",
+    "options": [
+    {
+    "a": "15",
+    "b": "12",
+    "c": "11",
+    "d": "9",
+    }
+    ],
+    "answer": "12",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "What is the name of a triangle that has two sides of the same length?",
+    "options": [
+    {
+    "a": "isosceles",
+    "b": "scalene",
+    "c": "obtuse",
+    "d": "acute",
+    }
+    ],
+    "answer": "isosceles",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": "All natural numbers are whole numbers. However, not all whole numbers can be considered natural ",
+    "options": [
+    {
+    "a": "True",
+    "b": "False",
+    }
+    ],
+    "answer": "True",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "You are given a 3-digit number. The second digit of this number is four times bigger than the third digit. The first digit of the number is 3 less than the second digit. What is the number? ",
+    "options": [
+    {
+    "a": "151",
+    "b": "160",
+    "c": "141",
+    "d": " 200",
+    }
+    ],
+    "answer": "141",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": " How many hours are there in a year (rounded to the nearest hour)?",
+    "options": [
+    {
+    "a": "8720",
+    "b": "8760",
+    "c": "9000",
+    "d": "None of the above."
+    }
+    ],
+    "answer": "8760",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "A man is currently 4 times older than his son. In 10 years, the man will be three times older than his son. What is the man’s age presently?",
+    "options": [
+    {
+    "a": "90",
+    "b": "80",
+    "c": "70",
+    "d": "50",
+    }
+    ],
+    "answer": "80",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "Which number is best represented in binary as 100? ",
+    "options": [
+    {
+    "a": "3",
+    "b": "4",
+    "c": "6"
+    }
+    ],
+    "answer": "4",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "What 3 numbers result in the same answer when they are added or multiplied altogether? ",
+    "options": [
+    {
+    "a": "1,2 and 4.",
+    "b": "2,4 and 8.",
+    "c": "3,4 and 6.",
+    "d": "1,2 and 3."
+    }
+    ],
+    "answer": "1,2 and 3.",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "Sarah read a novel in 20 days. She made it a habit to read 16 pages every day for the first 15 days. She then started reading 18 pages every day for the last 5 days. How many pages was the book? .",
+    "options": [
+    {
+    "a": "320 pages.",
+    "b": "300 pages.",
+    "c": "330 pages."
+    }
+    ],
+    "answer": "330 pages.",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "27 is a perfect cube. If true then what is the perfect cube of 27? ",
+    "options": [
+    {
+    "a": "9",
+    "b": "6",
+    "c": "3"
+    }
+    ],
+    "answer": "3",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": " An equilateral triangle has two of its sides equal. ",
+    "options": [
+    {
+    "a": "True",
+    "b": "False",
+   
+    }
+    ],
+    "answer": "False",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "Icosahedrons have how many equal sides? ",
+    "options": [
+    {
+    "a": "30",
+    "b": "20",
+    "c": "25",
+    "d": "60"
+    }
+    ],
+    "answer": "20",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": "The use of Arabic Numerals is invented in what country?    ",
+    "options": [
+    {
+    "a": "India",
+    "b": "Saudi Arabia",
+    "c": "Dubai",
+    "d": "Iran"
+    }
+    ],
+    "answer": "India",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "What does the term ‘crore’ mean? ",
+    "options": [
+    {
+    "a": "Four million",
+    "b": "Three million",
+    "c": "Ten million"
+    }
+    ],
+    "answer": "Ten million",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "Apart from one, I am the first positive number that is both a square and a cube. ",
+    "options": [
+    {
+    "a": "24",
+    "b": "54",
+    "c": "64",
+    }
+    ],
+    "answer": "64",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": "What does the term “Giga” refer to? ",
+    "options": [
+    {
+    "a": "One Billion",
+    "b": "One million",
+    "c": "One trillion"
+    }
+    ],
+    "answer": "One Billion",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 21,
+    "question": "Calculate the quotient of 150 and 3. ",
+    "options": [
+    {
+    "a": "450",
+    "b": "50",
+    "c": "100",
+    }
+    ],
+    "answer": "50",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 22,
+    "question": "What is the only prime number that is even? ",
+    "options": [
+    {
+    "a": "2",
+    "b": "4",
+    "c": "6",
+    }
+    ],
+    "answer": "2",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 23,
+    "question": "Which number is equivalent to 3^(4)÷3^(2)?",
+    "options": [
+    {
+    "a": "6",
+    "b": "9",
+    "c": "8",
+    "d": "5"
+    }
+    ],
+    "answer": "9",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 24,
+    "question": "I am an odd number. Take away one letter and I become even. What number am I?",
+    "options": [
+    {
+    "a": "seven",
+    "b": "five",
+    "c": "six"
+    }
+    ],
+    "answer": "seven",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 25,
+    "question": "How many feet are in a mile? ",
+    "options": [
+    {
+    "a": "5280",
+    "b": "5000",
+    "c": "5600",
+    }
+    ],
+    "answer": "5280",
+    "score": 0,
+    "status": ""
+    }
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>Score:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
